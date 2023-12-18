8bc2d1b0be3dd8666d186e32a645584c1c644286
laisbenb <woodipsy@gmail.com>
2023-12-18 15:48:08 +0100

    Second section grid finnished

--- static/js/main.js
+++ static/js/main.js
@@ -2,15 +2,13 @@ const randomNumber = Math.floor(Math.random() * 6) + 1;
 
 function getLogo() {
     let image = document.getElementById('gf-logo');
-    let html = "";
-    html += `<a href="index.html"><img src="static/img/Gentse Feesten Logos/GF-logo-2023-${randomNumber}.svg" alt="Gentse feesten logo"></a>`;
+    const html = `<a href="index.html"><img src="static/img/Gentse Feesten Logos/GF-logo-2023-${randomNumber}.svg" alt="Gentse feesten logo"></a>`;
     image.innerHTML = html;
 };
 
 function getImage() {
     let image = document.getElementById('gf-image');
-    let html = "";
-    html += `<img src="static/img/Gentse Feesten Logos/campagne-${randomNumber}.png" alt="Gentse fees ten image">`;
+    const html = `<img src="static/img/Gentse Feesten Logos/campagne-${randomNumber}.png" alt="Gentse fees ten image">`;
     image.innerHTML = html;
 }
 
