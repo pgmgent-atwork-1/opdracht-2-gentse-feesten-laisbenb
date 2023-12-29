3e24743d0b7c0c5152db99ce54b3f24d7914e934
laisbenb <woodipsy@gmail.com>
2023-12-29 16:15:00 +0100

    event cards done (carousel)

--- static/js/main.js
+++ static/js/main.js
@@ -30,10 +30,12 @@ async function getEventItems() {
 function generateEventItems(eventItems) {
     //day , day_of_week , start , title , location , month=juli , image
     let html = "";
+    let checkNumber = -1;
     for (let i = 0; i < 8; i++) {
-        const randomEventNumber = Math.floor(Math.random() * 500);
-        let checkNumber = -1;
-        if (checkNumber != randomEventNumber) {
+        console.log(`Checknumber start = ${checkNumber}`);
+        let randomEventNumber = Math.floor(Math.random() * 500);
+        console.log(randomEventNumber);
+        if (checkNumber != randomEventNumber && eventItems[randomEventNumber].image != null) {
             let event = document.getElementById('events');
         html += `<article class="article-event relative">
         <div class="artist-image">
@@ -48,9 +50,26 @@ function generateEventItems(eventItems) {
         </div>
         </article>`;
         event.innerHTML = html;
+        } else {
+            randomEventNumber = Math.floor(Math.random() * 500);
+            let event = document.getElementById('events');
+        html += `<article class="article-event relative">
+        <div class="artist-image">
+            <img src="${eventItems[randomEventNumber].image.full}" alt="Image of event">
+        </div>
+        <div class="event-date">
+            <p>${eventItems[randomEventNumber].day_of_week} ${eventItems[randomEventNumber].day} juli</p>
+        </div>
+        <div class="artist-information">
+            <h2>${eventItems[randomEventNumber].title}</h2>
+            <p><span class="red-box">${eventItems[randomEventNumber].location}</span> ${eventItems[randomEventNumber].start} u </p>
+        </div>
+        </article>`;
+        event.innerHTML = html;
+        console.log(`Second random number check = ${checkNumber}`);
         };
         checkNumber = randomEventNumber;
-        console.log(randomEventNumber);
+        console.log(`Checknumber end = ${checkNumber}`);
     };
     // BUGS: null niet gefiltered !
     // HErschrijf deze code met .slice() method.
