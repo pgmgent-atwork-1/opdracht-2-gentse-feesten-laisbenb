8a30a5fa052afc77d34fc5c216642676717f5405
Epictrepic <laith.benbelgacem@gmail.com>
2024-01-10 12:25:05 +0100

    search page attempt 1

--- static/js/searchPage.js
+++ static/js/searchPage.js
@@ -1,35 +1,35 @@
 /* ---------------- JSON FILE READER ---------------- */
 
-import {getEvents} from "./fetchData.js";
+//import {getEvents} from "./fetchData.js";
+function fetchData() {
+    fetch('https://www.pgm.gent/data/gentsefeesten/events_500.json')
+        .then(response => response.json());
+};
 
 /* ---------------- LOAD IN EVENTS ---------------- */
-async function loadEvents() {
-    const eventItems = await getEvents();
-    const searchTerm = document.getElementById('search-bar').value.toLowerCase();
-    search(eventItems, searchTerm);
 
-    console.log(eventItems);
-};
+document.getElementById("search-button").addEventListener("click", function() {
+    search();
+});
 
-function search(events, searchTerm) {
-    const resultBox = document.getElementById('search-results');
-    resultBox.innerHTML = "";
+function renderData(data) {
+    const resultContainer = document.getElementById('search-results');
+    resultContainer.innerHTML = "";
 
-    const filteredEvents = events.filter(event => {
-        return event.title.toLowerCase().includes(searchTerm)
+    data.forEach(item => {
+        const itemElement = document.createElement('div');
+        itemElement.textContent =  item.title;
+        resultContainer.appendChild(itemElement);
     });
-
-    if (filteredEvents.length > 0) {
-        filteredEvents.forEach(event => {
-            const eventContainer = document.createElement('div');
-            eventContainer.textContent = `${event.title}`;
-            resultBox.appendChild(eventContainer);
-        });
-    } else {
-        resultBox.textContent = 'Geen evenementen gevonden';
-    }
-    //console.log(eventItems);
-    //console.log("OBAMA A NIFFOOOW");
 };
 
-loadEvents();
+
+function search() {
+    const searchTerm = document.getElementById('search-bar').value.toLowerCase();
+    fetchData()
+        .then(data => {
+            renderData(data.filter(item => item.title.toLowerCase().includes(searchTerm)))
+        }).catch(error => console.error('Error fetching data:', error));
+};
+
+fetchData().then(data => renderData(data));
