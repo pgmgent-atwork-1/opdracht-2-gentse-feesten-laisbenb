fd68d2ec2a6675f8e70fad2ead9836a622881b60
Epictrepic <laith.benbelgacem@gmail.com>
2024-01-10 17:19:39 +0100

    number of results displayed

--- static/js/searchPage.js
+++ static/js/searchPage.js
@@ -5,7 +5,7 @@
 function fetchData() {
     return fetch('https://www.pgm.gent/data/gentsefeesten/events_500.json')
         .then(response => response.json());
-}
+};
 
 /* ---------------- DISPLAY EVENTS ---------------- */
 
@@ -22,21 +22,28 @@ function renderData(data) {
     const resultContainer = document.getElementById('search-results');
     resultContainer.innerHTML = "";
 
+    const resultTrackerContainer = document.getElementById('number-of-results');
+    let resultTracker = 0;
+
     data.forEach(item => {
+        resultTracker++;
         const itemElement = document.createElement('div');
         itemElement.innerHTML =  `<article class="article-event search-article relative">
-        <img src="${item.image.thumb}" alt="Event Image">
+        <img src="${item.image.full}" alt="Event Image">
         <div class="event-date">
-            <p>${item.day_of_week.slice(0, 2)} juli</p>
+        <p>${item.day_of_week.slice(0, 2)} juli</p>
         </div>
         <div class="artist-information">
-            <h2>${item.title}</h2>
-            <p><span class="red-box">${item.location}</span> ${item.start} u</p>
+        <h2>${item.title}</h2>
+        <p><span class="red-box">${item.location}</span> ${item.start} u</p>
         </div>
         </article>` ;
         resultContainer.appendChild(itemElement);
     });
-}
+
+    resultTrackerContainer.innerHTML = `<p><strong>${resultTracker} resultaten gevonden</strong></p>`;
+    console.log(`${resultTracker} results found`);
+};
 
 function search() {
     const searchTerm = document.getElementById('search-bar').value.toLowerCase();
@@ -45,6 +52,6 @@ function search() {
             renderData(data.filter(item => item.title.toLowerCase().includes(searchTerm)))
         })
         .catch(error => console.error('Error fetching data:', error));
-}
+};
 
 fetchData().then(data => renderData(data));
