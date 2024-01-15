2e7a4041d92577048e2a4a9803519e9efa2e1ea5
laisbenb <147707118+laisbenb@users.noreply.github.com>
2024-01-15 15:04:51 +0100

    Day page images and logos linked

--- static/js/loadGFLogos.js
+++ static/js/loadGFLogos.js
@@ -5,9 +5,9 @@ function getLogo() {
     const secondLogo = document.getElementById('gf-footer-logo');
     const thirdLogo = document.getElementById('gf-menu-logo');
     const logoSrc = `static/img/Gentse Feesten Logos/GF-logo-2023-${randomNumber}.svg`;
-    firstLogo.src = logoSrc;
-    secondLogo.src = logoSrc;
-    thirdLogo.src = logoSrc;
+    firstLogo.src += logoSrc;
+    secondLogo.src += logoSrc;
+    thirdLogo.src += logoSrc;
 };
 
 function getImage() {
@@ -16,10 +16,10 @@ function getImage() {
     const thirdImage = document.getElementById('gf-menu-image');
     const imageSrc = `static/img/Gentse Feesten Logos/campagne-${randomNumber}.png`;
     if (firstImage) {
-        firstImage.src = imageSrc;
+        firstImage.src += imageSrc;
     };
-    secondImage.src = imageSrc;
-    thirdImage.src = imageSrc;
+    secondImage.src += imageSrc;
+    thirdImage.src += imageSrc;
 };
 
 getLogo();
