39e875369e77cb02494cce57a17fd0eaba201ae8
laisbenb <147707118+laisbenb@users.noreply.github.com>
2024-01-19 09:28:39 +0100

    dropdown made with js

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -1,3 +1,11 @@
-function toggleDropdown() {
-    const dropdownBtn = document.getElementById('dropdown-button');
-};
+document.addEventListener("DOMContentLoaded", () => {
+    const dropdownBtn = document.getElementById('dropdownBtn');
+    const dropdownMenu = document.querySelector('.dropdown-content');
+
+    console.log('dropdownBtn:', dropdownBtn);
+    console.log('dropdownMenu:', dropdownMenu);
+
+    dropdownBtn.addEventListener("click", function() {
+      dropdownMenu.classList.toggle('showDropDown');
+    });
+});
