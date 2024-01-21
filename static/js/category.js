734a94a3ae35efb261caf572887f93a57cfc4e35
Epictrepic <laith.benbelgacem@gmail.com>
2024-01-21 16:01:43 +0100

    Day page all events sorted and displayed

--- static/js/category.js
+++ static/js/category.js
@@ -13,13 +13,23 @@ function collectEventCategories(eventData) {
             categoryArray.push(...event.category);
     };
     let filteredCategoryArray = [...new Set(categoryArray)];
-    console.log(filteredCategoryArray);
-    console.log(filteredCategoryArray.length);
 
     for (let i = 0; i < filteredCategoryArray.length; i++) {
+        const category = filteredCategoryArray[i];
         document.getElementById('categories').innerHTML  += `<div class="flex one-category">
-        <img class="tag-svg" src="../static/img/gentse-feesten-icoontjes/tag.svg" alt="Tag icon"> <p class="black">${filteredCategoryArray[i]}</p>
+        <img class="tag-svg" src="../static/img/gentse-feesten-icoontjes/tag.svg" alt="Tag icon"> <p class="black">${category}</p>
         </div>`;
+        const eventContentID = `eventContent_${i}`;
+        document.getElementById('categoriesAndEvents').innerHTML += `<h2>${category}</h2>
+        <div id="${eventContentID}"></div>`;
+        console.log(category);
+        for (const event of eventData) {
+            if (event.category.includes(category)) {
+                //console.log(event.category.includes(filteredCategoryArray[i]));
+                console.log(event.category);
+                document.getElementById(eventContentID).innerHTML += `<p>${event.title}</p>`
+            };
+        };
     };
 };
 
