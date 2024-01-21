87abc2b8a68abf77fce3bf86f1614c4b573a92b8
Epictrepic <laith.benbelgacem@gmail.com>
2024-01-21 18:22:57 +0100

    Scroll to category done

--- static/js/category.js
+++ static/js/category.js
@@ -20,21 +20,18 @@ function collectEventCategories(eventData, filterDay) {
         const category = filteredCategoryArray[i];
         const eventContentID = `eventContent_${i}`;
         document.getElementById('categories').innerHTML  += `<div class="flex one-category">
-        <img class="tag-svg" src="../static/img/gentse-feesten-icoontjes/tag.svg" alt="Tag icon"> <p class="black">${category}</p>
+        <img class="tag-svg" src="../static/img/gentse-feesten-icoontjes/tag.svg" alt="Tag icon"> <a href="#" class="scroll-links black" data-target="event-title-${i}">${category}</a>
         </div>`;
-        document.getElementById('categoriesAndEvents').innerHTML += `<h2 class="category-titles container">${category}</h2>
+        document.getElementById('categoriesAndEvents').innerHTML += `<h2 id="event-title-${i}" class="category-titles container">${category}</h2>
         <div id="${eventContentID}" class="event-category-content"></div>`;
-        console.log(category);
+        //console.log(category);
         for (const event of eventData) {
             if (event.category.includes(category) && (event.day === filterDay)) {
                 //console.log(event.category.includes(filteredCategoryArray[i]));
-                console.log(event.category);
+                //console.log(event.category);
                 document.getElementById(eventContentID).innerHTML += `<article class="article-event relative">
                 <div class="artist-image">
                     <img src="${event.image.full}" alt="Image of event">
-                </div>
-                <div class="event-date">
-                    <p>${event.day_of_week.slice(0, 2)} ${event.day} juli</p>
                 </div>
                 <div class="artist-information">
                     <h2>${event.title}</h2>
@@ -44,6 +41,25 @@ function collectEventCategories(eventData, filterDay) {
             };
         };
     };
+    const scrollLinks = document.querySelectorAll('.scroll-links');
+    console.log(scrollLinks);
+    scrollLinks.forEach((link) => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            console.log("Link clicked");
+
+            const scrollToLocation = link.getAttribute('data-target');
+            console.log("Scroll to location:", scrollToLocation);
+            const scrollTo = document.getElementById(scrollToLocation);
+            console.log("Scroll to element:", scrollTo);
+            if (scrollTo) {
+                window.scrollTo({
+                    top: scrollTo.offsetTop,
+                    behavior: 'smooth'
+                });
+            };
+        });
+    });
     console.log(`Filtered events for day ${filterDay}`);
 };
 
