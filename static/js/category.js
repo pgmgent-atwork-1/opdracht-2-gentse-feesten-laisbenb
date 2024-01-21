02c93e95ad6ed2e9936b95dd32f79c8f442ddd00
Epictrepic <laith.benbelgacem@gmail.com>
2024-01-21 17:09:07 +0100

    Filter by day with URLparams

--- static/js/category.js
+++ static/js/category.js
@@ -4,10 +4,12 @@ import {getEvents} from "./fetchData.js";
 
 async function fetchEventCategories() {
     let eventData = await getEvents();
-    collectEventCategories(eventData);
+    const urlParams = new URLSearchParams(window.location.search);
+    const filterDay = urlParams.get('day');
+    collectEventCategories(eventData, filterDay);
 };
 
-function collectEventCategories(eventData) {
+function collectEventCategories(eventData, filterDay) {
     let categoryArray = [];
     for (const event of eventData) {
             categoryArray.push(...event.category);
@@ -16,21 +18,33 @@ function collectEventCategories(eventData) {
 
     for (let i = 0; i < filteredCategoryArray.length; i++) {
         const category = filteredCategoryArray[i];
+        const eventContentID = `eventContent_${i}`;
         document.getElementById('categories').innerHTML  += `<div class="flex one-category">
         <img class="tag-svg" src="../static/img/gentse-feesten-icoontjes/tag.svg" alt="Tag icon"> <p class="black">${category}</p>
         </div>`;
-        const eventContentID = `eventContent_${i}`;
-        document.getElementById('categoriesAndEvents').innerHTML += `<h2>${category}</h2>
-        <div id="${eventContentID}"></div>`;
+        document.getElementById('categoriesAndEvents').innerHTML += `<h2 class="category-titles container">${category}</h2>
+        <div id="${eventContentID}" class="event-category-content"></div>`;
         console.log(category);
         for (const event of eventData) {
-            if (event.category.includes(category)) {
+            if (event.category.includes(category) && (event.day === filterDay)) {
                 //console.log(event.category.includes(filteredCategoryArray[i]));
                 console.log(event.category);
-                document.getElementById(eventContentID).innerHTML += `<p>${event.title}</p>`
+                document.getElementById(eventContentID).innerHTML += `<article class="article-event relative">
+                <div class="artist-image">
+                    <img src="${event.image.full}" alt="Image of event">
+                </div>
+                <div class="event-date">
+                    <p>${event.day_of_week.slice(0, 2)} ${event.day} juli</p>
+                </div>
+                <div class="artist-information">
+                    <h2>${event.title}</h2>
+                    <p><span class="red-box">${event.location}</span> ${event.start} u </p>
+                </div>
+                </article>`;
             };
         };
     };
+    console.log(`Filtered events for day ${filterDay}`);
 };
 
 fetchEventCategories();
